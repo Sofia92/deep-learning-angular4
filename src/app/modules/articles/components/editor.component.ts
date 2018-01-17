48f99f8ff285de3d7b90602df02b82331db4eb57
Sofia92 <1157335914@qq.com>
2018-01-17 19:57:03 +0800

    import marked.js to app

--- src/app/modules/articles/components/editor.component.ts
+++ src/app/modules/articles/components/editor.component.ts
@@ -5,6 +5,20 @@
  * Description:
  */
 import {Component} from '@angular/core';
+import * as marked from 'marked';
+
+marked.setOptions({
+  renderer: new marked.Renderer(),
+  gfm: true,
+  tables: true,
+  breaks: false,
+  pedantic: false,
+  sanitize: false,
+  smartLists: true,
+  smartypants: false
+});
+
+
 @Component({
   selector: 'app-article-editor',
   templateUrl: './editor.component.html',
@@ -12,8 +26,16 @@ import {Component} from '@angular/core';
 })
 
 export class ArticleEditorComponent {
+  content: any;
+  markedContent: any;
 
   back() {
     window.history.back();
   }
+
+  displayMarkedContent(content) {
+    this.content = content;
+    this.markedContent = marked(content);
+  }
+
 }
